96ffd27975f68b48a537a8afb046152582b8b061
Nando San Miguel <contact@namoku.xyz>
2022-04-18 13:51:52 -0500

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { Header } from './components';
 
 function App() {
-  return <div>Prueba</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+
+function LinkButton({ to, value }: { to: string; value: string }) {
+  return (
+    <Link to={to}>
+      <button className="linkButton" type="button">
+        {value}
+      </button>
+    </Link>
+  );
+}
+
+function Header() {
+  return (
+    <div className="container">
+      <h1>CodeQuiz</h1>
+      <p>by Namoku</p>
+      <div className="navContainer">
+        <LinkButton to="/" value="Home" />
+        <LinkButton to="/play" value="Play" />
+        <LinkButton to="/login" value="Login" />
+        <LinkButton to="/register" value="Register" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;

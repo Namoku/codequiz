2d8e429b0f3799973065c3c6a62f2851f3d47ce2
Nando San Miguel <contact@namoku.xyz>
2022-04-26 23:03:32 -0500

    Fix navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom';
 
 function LinkButton({ to, value }: { to: string; value: string }) {
   return (
-    <li>
-      <Link to={to}>
-        <button className="linkButton" type="button">
-          {value}
-        </button>
-      </Link>
-    </li>
+    <Link to={to}>
+      <button className="linkButton" type="button">
+        {value}
+      </button>
+    </Link>
   );
 }
 
@@ -17,12 +15,12 @@ function Header() {
   return (
     <div className="container">
       <h1>CodeQuiz</h1>
-      <ul className="navContainer">
+      <article className="navContainer">
         <LinkButton to="/" value="Home" />
         <LinkButton to="/play" value="Play" />
         <LinkButton to="/login" value="Login" />
         <LinkButton to="/register" value="Register" />
-      </ul>
+      </article>
     </div>
   );
 }
